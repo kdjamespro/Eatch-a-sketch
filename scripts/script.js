4d58edfef530723388f805b83505d14ef9f6ffe9
Kenneth Dwight James L. Probadora <jamesprobadora@gmail.com>
2021-08-19 22:27:23 +0800

    Add color picker and drag click functionality to color the tiles

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const drawingBoard = document.querySelector('.drawing-board');
+let drag = false;
+let color = document.querySelector('#color-picker');
 
 function drawBoard(size)
 {
@@ -6,8 +8,29 @@ function drawBoard(size)
     {
         let tile = document.createElement('div');
         tile.classList.toggle('drawing-boxes');
+        tile.addEventListener('mousedown', (event) => fillTiles(event)
+        );
+        tile.addEventListener('mouseover', (event) => fillTiles(event));
         drawingBoard.appendChild(tile);
     }
 }
 
+function fillTiles(event)
+{
+    if(drag)
+    {
+        let color = document.querySelector('#color-picker').value;
+        event.target.style.background = color;
+    }
+}
+
+drawingBoard.addEventListener('mousedown', (event) =>{
+    drag = true;
+    fillTiles(event)
+});
+
+drawingBoard.addEventListener('mouseup', () =>{
+    drag = false;
+    console.log(drag)
+});
 drawBoard(16);
