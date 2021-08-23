fdbcd788195b6e7193ec2b184ea38aada093ad35
Kenneth Dwight James L. Probadora <jamesprobadora@gmail.com>
2021-08-23 22:02:38 +0800

    Add clear button and resizing slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 const drawingBoard = document.querySelector('.drawing-board');
+const clear = document.querySelector('.clear');
+
 let drag = false;
 let color = document.querySelector('#color-picker');
+let slider = document.querySelector('#grid-slider');
 
 function drawBoard(size)
 {
+    setSize(size);
     for(let i = 0; i < size * size; i++)
     {
         let tile = document.createElement('div');
@@ -12,6 +16,20 @@ function drawBoard(size)
         );
         tile.addEventListener('mouseover', (event) => fillTiles(event));
         drawingBoard.appendChild(tile);
+    }
+}
+
+function setSize(size)
+{
+    drawingBoard.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr);`)
+}
+
+function deleteBoard()
+{
+    let tiles = document.querySelector('.drawing-board').childNodes;
+    for(i = 0; tiles.length; i++)
+    {
+        tiles[i].parentNode.removeChild(tiles[i]);
     }
 }
 
@@ -33,4 +51,21 @@ drawingBoard.addEventListener('mouseup', () =>{
     drag = false;
     console.log(drag)
 });
-drawBoard(16);
+
+clear.addEventListener('click', () => {
+    let drawingTiles = document.querySelectorAll('.drawing-boxes');
+    if(!drawingTiles.length == 0)
+    {
+        for(i = 0; drawingTiles.length; i++)
+        {
+            drawingTiles[i].style.background = 'white';
+        }
+    }
+});
+
+slider.addEventListener('change', () =>{
+    deleteBoard();
+    drawBoard(parseInt(slider.value));
+})
+
+drawBoard(parseInt(slider.value));
